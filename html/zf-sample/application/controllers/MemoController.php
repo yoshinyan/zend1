59c0fbd5160e48840909b536d48a7a4ec3f12bbf
yoshinyan <yoshinyan@yoshinyannoMacBook-Pro.local>
2020-06-10 15:18:25 +0900

    update and delete

--- html/zf-sample/application/controllers/MemoController.php
+++ html/zf-sample/application/controllers/MemoController.php
@@ -16,8 +16,7 @@ class MemoController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $select = $this->db->select();
-        $select->from('memo', '*');
+        $select = $this->db->select()->from('memo', '*');
         $this->view->memos = $this->db->fetchAll($select);
     }
    
@@ -36,5 +35,29 @@ class MemoController extends Zend_Controller_Action
         $this->_helper->redirector('index', 'Memo');
     }
 
+    public function editAction()
+    {
+        $select = $this->db->select();
+        $select->from('memo', '*')->where('id = ?', $this->getRequest()->getQuery('id'));
+        $this->view->memo = $this->db->fetchRow($select);
+    }
+
+    public function updateAction()
+    {
+        $params = [
+            'title' => $this->getRequest()->getPost('title'),
+            'body' => $this->getRequest()->getPost('body'),
+        ];
+
+        $where = $this->db->quoteInto('id = ?' , $this->getRequest()->getPost('id'));
+        $this->db->update('memo', $params, $where);
+        $this->_helper->redirector('index', 'Memo');
+    }
+
+    public function deleteAction()
+    {
+        $this->db->delete('memo', $this->db->quoteInto('id = ?' , $this->getRequest()->getPost('id')));
+        $this->_helper->redirector('index', 'Memo');;
+    }
 }
 
